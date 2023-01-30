c845976f6e311f9233716cea8f9189159249673c
info <jaebum7396@naver.com>
2023-01-30 16:07:16 +0900

    feature. 게이트웨이 헤더 필터 추가

--- src/main/java/com/aflk/gateway/Application.java
+++ src/main/java/com/aflk/gateway/Application.java
@@ -2,10 +2,17 @@ package com.aflk.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	@Bean
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity security) {
+		return security.csrf().disable().build();
 	}
 }
