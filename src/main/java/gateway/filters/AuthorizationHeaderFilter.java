3e7745494eb874d670031b146d4b55d2c991a806
info <jaebum7396@naver.com>
2023-03-23 20:17:13 +0900

    feature. 인터셉터 추가 gateway 이외 접근 block

--- src/main/java/gateway/filters/AuthorizationHeaderFilter.java
+++ src/main/java/gateway/filters/AuthorizationHeaderFilter.java
@@ -1,8 +1,14 @@
 package gateway.filters;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+
 import org.apache.logging.log4j.util.Strings;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.core.env.Environment;
@@ -18,42 +24,63 @@ import reactor.core.publisher.Mono;
 @Component
 public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
     private final Environment environment;
+    
     public AuthorizationHeaderFilter(Environment environment) {
         super(Config.class);
         this.environment = environment;
     }
+    
     @Override
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
             System.out.println(request.getURI());
+            
             if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)){
                 return onError(exchange, "No authorization header", HttpStatus.UNAUTHORIZED);
             }
+            
             String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
             String jwt = authorizationHeader.replace("Bearer", "");
+            
+            System.out.println("reqJwt : " + jwt);
+            
             if (!isJwtValid(jwt)){
                 return onError(exchange, "JWT token is not valid", HttpStatus.UNAUTHORIZED);
             }
+            
             return chain.filter(exchange);
         };
     }
+    
     private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
         ServerHttpResponse response = exchange.getResponse();
         response.setStatusCode(httpStatus);
         log.error(err);
         return response.setComplete();
     }
+    
     private boolean isJwtValid(String jwt) {
+    	boolean returnValue = true;
         String subject = null;
+        String salt = environment.getProperty("jwt.secret.key");
+        Key secretKey = Keys.hmacShaKeyFor(salt.getBytes(StandardCharsets.UTF_8));
         try {
-            subject = Jwts.parser().setSigningKey(environment.getProperty("token.secret"))
+            subject = 
+            		//Jwts.parser().setSigningKey(environment.getProperty("jwt.secret.key"))
+            		Jwts.parserBuilder().setSigningKey(secretKey).build()
                     .parseClaimsJws(jwt).getBody()
                     .getSubject();
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        } catch (Exception exception) {
+            returnValue = false;
         }
-        return !Strings.isBlank(subject);
+
+        if (subject == null || subject.isEmpty()) {
+            returnValue = false;
+        }
+
+        return returnValue;
     }
+    
     public static class Config {}
 }
